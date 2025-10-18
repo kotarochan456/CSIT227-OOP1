849cb333779971e34ea4d8f77e7656fcc69e46ec
kotarochan456 <lanceangelescobanas@gmail.com>
2025-10-18 22:07:42 +0800

    updated to 3v3 versus mode, fixed the healing ability mechanism

--- src/Skill.java
+++ src/Skill.java
@@ -1,13 +1,21 @@
+package Game;
+
 public class Skill {
     private String name;
     private int damage;
     private int cooldown;
     private int currentCooldown;
+    private boolean isHealing;
 
     public Skill(String name, int damage, int cooldown) {
+        this(name, damage, cooldown, false);
+    }
+
+    public Skill(String name, int damage, int cooldown, boolean isHealing) {
         this.name = name;
         this.damage = damage;
         this.cooldown = cooldown;
+        this.isHealing = isHealing;
         this.currentCooldown = 0;
     }
 
@@ -16,26 +24,28 @@ public class Skill {
             System.out.println(name + " is on cooldown for " + currentCooldown + " more turns!");
             return 0;
         }
+
         currentCooldown = cooldown;
-        System.out.println("Used " + name + "! Dealt " + damage + " damage!");
-        return damage;
+
+        if (isHealing) {
+            System.out.println("✨ Used " + name + "! Healed allies for " + damage + " HP!");
+            return -damage;
+        } else {
+            System.out.println("🔥 Used " + name + "! Dealt " + damage + " damage!");
+            return damage;
+        }
     }
 
     public void reduceCooldown() {
-        if (currentCooldown > 0) {
-            currentCooldown--;
-        }
+        if (currentCooldown > 0) currentCooldown--;
     }
-    public String getStatus() {
-        if (currentCooldown == 0) {
-            return name + " (Ready!)";
-        } else {
-            return name + " (CD: " + currentCooldown + ")";
-        }
-    }
-
 
     public String getName() { return name; }
     public int getDamage() { return damage; }
-    public int getCooldown() { return cooldown; }
+    public boolean isHealing() { return isHealing; }
+
+    public String getStatus() {
+        return name + (currentCooldown > 0 ? " (CD: " + currentCooldown + ")" : " (Ready)");
+    }
 }
+
